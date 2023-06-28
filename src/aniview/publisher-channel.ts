192e3087531b4766958bc12f5e51cebf163555b1
vyasinskiy <yasinsky93@gmail.com>
2023-06-28 16:48:44 +0400

    Update publisher-channels-types

--- src/aniview/publisher-channel.ts
+++ src/aniview/publisher-channel.ts
@@ -5,51 +5,76 @@ export enum PublisherChannelStatus {
 
 export interface PublisherChannel {
   instanceId: string;
-  optimized: unknown;
+  optimized: boolean;
   optModel: unknown;
   optModelV: unknown;
-  accounting: unknown;
-  revShare: unknown;
-  ivt: unknown;
-  fp: unknown;
-  sid: unknown;
+  accounting?: {
+    type: number;
+    enabled: unknown;
+    value: number;
+    max: unknown;
+    cutModel: unknown;
+  };
+  revShare?: number;
+  ivt?: {
+    prebid?: {
+      active: boolean;
+      action: unknown;
+      level: unknown;
+      supplier: number;
+      sampling: unknown;
+    };
+    postbids: unknown[];
+  };
+  fp?: number;
+  sid?: string;
   nlasid: unknown;
-  adSources: [
-    {
-      id: string;
-      priority: number;
-    },
-  ];
-  trackers: unknown;
+  adSources: {
+    id: string;
+    priority: number;
+  }[];
+  trackers?: unknown[];
   impcoc: unknown;
-  environment: unknown;
-  adTxt: unknown;
-  ovrdPbSchain: unknown;
-  ovrdParSchain: unknown;
+  environment?: number;
+  adTxt: boolean;
+  ovrdPbSchain?: boolean;
+  ovrdParSchain?: boolean;
   syncChannel: unknown;
-  type: number;
-  optData: unknown;
-  publisher: unknown;
+  type?: number;
+  optData?: {
+    fr: number;
+    request: number;
+    opportunity: number;
+    impression: number;
+    inventory: number;
+  };
+  publisher?: string;
   playerSettings: unknown;
   maxrun: number;
   maximp: number;
   interval: number;
   custom: unknown;
-  minRpm: number;
-  limitAdvertisers: unknown;
-  limitAdvertisersList: unknown;
+  minRpm?: number;
+  limitAdvertisers?: boolean;
+  isSessionIdEnabled: unknown;
+  limitAdvertisersList?: unknown[];
   adstxt: unknown;
   timeoutMs: unknown;
-  enableMbq: boolean;
-  mbq: unknown;
-  prp: unknown;
+  enableMbq?: boolean;
+  mbq?: number;
+  prp?: {
+    prs: unknown[];
+    mm?: number;
+    cmm?: number;
+  };
   status: PublisherChannelStatus;
   name: string;
   description: string;
   publisherId: string;
-  restoreFrom: unknown;
+  restoreFrom?: string;
+  creativeType: number;
   adsourceOriginId: unknown;
-  lastAuditTrailId: unknown;
+  lastAuditTrailId?: string;
   id: unknown;
   _id: {
     $oid: string;
